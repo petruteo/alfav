08ae06239a818e5e8231992b3019a71bf54df53d
petruteo <escu.petru@gmail.com>
2018-08-31 17:38:05 +0300

    de la cristiana add

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-var theURL = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=1min&apikey=demo";
+// var theURL = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=1min&apikey=30Y6LPCCQV16RA06";
+var theURL = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=FB&interval=1min&apikey=30Y6LPCCQV16RA06";
 
-$(document).ready(function() {
+
+$(document).ready(function () {
   $("#stockIndicator").show();
   doAjax(theURL);
 
-  $('.ajaxtrigger').click(function() {
+  $('.ajaxtrigger').click(function () {
     $("#stockIndicator").show();
     doAjax(theURL);
     return false;
@@ -19,16 +21,35 @@ $(document).ready(function() {
       url: url,
       dataType: 'json',
       contentType: "application/json",
-      success: function(data) {
+      success: function (data) {
 
         var symbol = data['Meta Data']['2. Symbol']
         var lastRefreshed = data['Meta Data']['3. Last Refreshed']
-        var lastTradePriceOnly = data['Time Series (1min)'][lastRefreshed]['4. close']
+        var lastTradePriceOnly = data['Time Series (1min)']
+
+        var timeSeries = data['Time Series (1min)'];
+        var result = [];
+        var closeEntries = [];
+
+        for (var key in timeSeries) {
+          if (timeSeries.hasOwnProperty(key)) {
+            result.push(timeSeries[key]);
+          }
+        }
+
+        for (i = 0; i < result.length; i++) {
+          closeEntries.push(result[i][Object.keys(result[i])[3]])
+        }
+
+        console.log(closeEntries);
+        console.log(result);
+
         var lastVolume = data['Time Series (1min)'][lastRefreshed]['5. volume']
+        //console.log(lastTradePriceOnly);
 
         $('#stockSymbol').html(symbol);
         $('#stockAsk').html(lastTradePriceOnly);
-        $('#stockVolume').html(numberWithCommas(lastVolume));
+        $('#stockVolume').html(lastVolume);
         $("#stockIndicator").hide();
       }
     });
