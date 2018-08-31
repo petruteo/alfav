90aa7c375c530a9bc6183bcb4ed376b28bcf4140
petruteo <escu.petru@gmail.com>
2018-08-31 22:57:50 +0300

    mkt adaugat si importit in functii

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 // var theURL = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=1min&apikey=30Y6LPCCQV16RA06";
 var theURL = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=FB&interval=1min&apikey=30Y6LPCCQV16RA06";
-
+var theMarketURL = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=VOO&interval=1min&apikey=30Y6LPCCQV16RA06";
 
 $(document).ready(function () {
   $("#stockIndicator").show();
-  doAjax(theURL);
+  var stockValuesArray = doAjax(theURL);
+  var stockMarketArray = doMarketAjax(theMarketURL);
+
+  console.log("Stock value", stockValuesArray);
+  console.log("market index ETF = VOO:", stockMarketArray);
+
+
 
   $('.ajaxtrigger').click(function () {
     $("#stockIndicator").show();
@@ -16,6 +22,24 @@ $(document).ready(function () {
     return x.toString().replace(/\B(?=(?:\d{3})+(?!\d))/g, ",");
   }
 
+  function getValuesFromData(data) {
+    var timeSeries = data['Time Series (1min)'];
+    var result = [];
+    var closeEntries = [];
+
+    for (var key in timeSeries) {
+      if (timeSeries.hasOwnProperty(key)) {
+        result.push(timeSeries[key]);
+      }
+    }
+
+    for (i = 0; i < result.length; i++) {
+      closeEntries.push(parseFloat(result[i][Object.keys(result[i])[3]]).toFixed(4))
+    }
+    // console.log("+++++", result);
+    return closeEntries.reverse();
+  }
+
   function doAjax(url) {
     $.ajax({
       url: url,
@@ -25,24 +49,11 @@ $(document).ready(function () {
 
         var symbol = data['Meta Data']['2. Symbol']
         var lastRefreshed = data['Meta Data']['3. Last Refreshed']
-        var lastTradePriceOnly = data['Time Series (1min)']
+        var lastTradePriceOnly = data['Time Series (1min)'][lastRefreshed]['4. close']
 
-        var timeSeries = data['Time Series (1min)'];
-        var result = [];
-        var closeEntries = [];
 
-        for (var key in timeSeries) {
-          if (timeSeries.hasOwnProperty(key)) {
-            result.push(timeSeries[key]);
-          }
-        }
-
-        for (i = 0; i < result.length; i++) {
-          closeEntries.push(result[i][Object.keys(result[i])[3]])
-        }
-
-        console.log(closeEntries);
-        console.log(result);
+        // console.log("*****", getValuesFromData(data));
+        // console.log(data);
 
         var lastVolume = data['Time Series (1min)'][lastRefreshed]['5. volume']
         //console.log(lastTradePriceOnly);
@@ -51,7 +62,27 @@ $(document).ready(function () {
         $('#stockAsk').html(lastTradePriceOnly);
         $('#stockVolume').html(lastVolume);
         $("#stockIndicator").hide();
+        stockValuesArray = getValuesFromData(data);
+        console.log("** din functie stock", stockValuesArray);
+
       }
     });
   }
+
+
+  function doMarketAjax(url) {
+    $.ajax({
+      url: url,
+      dataType: 'json',
+      contentType: "application/json",
+      success: function (data) {
+        stockMarketArray = getValuesFromData(data);
+        console.log("** din functie market", stockMarketArray);
+
+        // return marketArray;
+
+      }
+    });
+  }
+
 });
